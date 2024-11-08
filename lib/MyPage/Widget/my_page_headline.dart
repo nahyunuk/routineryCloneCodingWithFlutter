65d42bd25ab4bca5d93f40ff935bc24fbf794b0b
nahyunuk <s24022@gsm.hs.kr>
2024-11-08 15:39:16 +0900

    feat: 헤드라인 위젯 추가

---
+++ lib/MyPage/Widget/my_page_headline.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+Widget createHeadline(String headline) {
+  return Text(
+    headline,
+    style: TextStyle(
+      fontFamily: 'LeagueSpartan',
+      fontSize: 30,
+    ),
+  );
+}
