b749f284b90c86d5d141fd1ec7c5bc890a1b0b2e
nahyunuk <s24022@gsm.hs.kr>
2024-11-08 15:34:05 +0900

    feat: 테마 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:routinery_clone_coding_with_flutter/MyPage/my_page.dart';
+import 'package:routinery_clone_coding_with_flutter/myapp.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +12,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: App(),
+    );
   }
 }
