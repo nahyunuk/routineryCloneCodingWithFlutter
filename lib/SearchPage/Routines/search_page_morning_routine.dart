f409e981e46c4524ad6bf0cf4a479a5f94be812e
nahyunuk <s24022@gsm.hs.kr>
2024-11-14 20:35:45 +0900

    feat: 아침 루틴 추가

---
+++ lib/SearchPage/Routines/search_page_morning_routine.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:routinery_clone_coding_with_flutter/SearchPage/Widget/search_page_routines_container.dart';
+
+class SearchPageMorningRoutine extends StatefulWidget {
+  const SearchPageMorningRoutine({super.key});
+
+  @override
+  State<SearchPageMorningRoutine> createState() =>
+      _SearchPageMorningRoutineState();
+}
+
+class _SearchPageMorningRoutineState extends State<SearchPageMorningRoutine> {
+  final List<Map<String, dynamic>> routinesMap = [
+    {
+      'imageName': 'assets/images/sun_icon.png',
+      'routineTitle': '미라클 모닝',
+      'popular': false,
+      'beginner': false,
+    },
+    {
+      'imageName': 'assets/images/wake_up_icon.png',
+      'routineTitle': '모닝 루틴',
+      'popular': true,
+      'beginner': false,
+    },
+    {
+      'imageName': 'assets/images/water.png',
+      'routineTitle': '하루의 시작',
+      'popular': false,
+      'beginner': true,
+    },
+    {
+      'imageName': 'assets/images/goal_icon.png',
+      'routineTitle': '성장하는 아침',
+      'popular': false,
+      'beginner': false,
+    },
+
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+          height: 740,
+          child: ListView.builder(
+            physics: NeverScrollableScrollPhysics(),
+            itemCount: routinesMap.length,
+            itemBuilder: (context, index) {
+              return routinesContainer(
+                imageName: routinesMap[index]['imageName'],
+                routineTitle: routinesMap[index]['routineTitle'],
+                popular: routinesMap[index]['popular'],
+                beginner: routinesMap[index]['beginner'],
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
