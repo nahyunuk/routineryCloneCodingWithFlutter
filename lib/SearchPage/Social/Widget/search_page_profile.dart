65322f6566309739245a6281545cc4dc834f21b7
nahyunuk <s24022@gsm.hs.kr>
2024-11-16 09:05:19 +0900

    feat: 위젯 생성

---
+++ lib/SearchPage/Social/Widget/search_page_profile.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class SearchPageProfile extends StatelessWidget {
+  const SearchPageProfile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
